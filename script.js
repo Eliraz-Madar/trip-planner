dd430bdd90ae69f28338900f7e39c69bb6ce695a
Eliraz Madar <Eliraz.eliezer.Madar@s.afeka.ac.il>
2025-06-04 10:57:55 +0300

    client

---
+++ script.js
@@ -0,0 +1,13 @@
+// regitration
+  function registerUser(event) {
+        // Prevent the default link navigation until registration is processed
+        event.preventDefault();
+        
+        // Begin registration process
+        // Replace the alert with your actual registration process.
+        alert("Registration process started");
+
+        // After the registration process is complete
+        // You can then redirect to planner.html or any other page.
+        window.location.href = "planner.html";
+      }
